35d74a646084205433b5334dac62e203a5cef8b8
lify <lify@hzzh.com>
2020-09-15 17:52:31 +0800

    🔧

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { HotModuleReplacementPlugin } = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const webpack = require('webpack');
 
 module.exports = {
@@ -10,19 +11,25 @@ module.exports = {
     entry: './src/index.js', //指定构建入口
 
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'build.js'
+        path: path.resolve(__dirname, 'dist1'),
+        filename: '[name].js'
     },
     devServer: { 
         contentBase: path.resolve(__dirname, "dist"),
         compress: true,
         port: 8080,
-        open: true,
+        open: true, 
         hot: true, //开启热更新
     },
-    
+
+    // devtool: "cheap-module-source-map", // 显示源代码 
+    // devtool: "cheap-source-map", //显示转换后的代码
+    devtool: "eval", //显示打包后的代码
     optimization: {
-        usedExports: true
+        usedExports: true,
+        splitChunks: {
+            chunks: 'async'
+        }
     },
 
     plugins: [
@@ -32,6 +39,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: 'src/index.html', // 配置默认模板文件
         }),
+        new BundleAnalyzerPlugin(), // 
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NamedModulesPlugin()
     ],
